eddefd831dba27736338f59cd46e61d7012edc9f
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-08-22 02:27:54 +0800

    change footer social links from text to icons

---
+++ src/app/components/Home/works.jsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+
+export const Works = ({ id }) => {
+  return (
+    <section id={id}>
+      <div className="inner-container flex flex-col gap-32 items-center">
+        <div className="flex flex-row items-center justify-between gap-48">
+          <div className="flex flex-col gap-4 w-3/5">
+            <h3>
+              Your business is not a template, then why should your website be
+              one?
+            </h3>
+            <div className="flex flex-col gap-6">
+              <h2>Solutions that convert</h2>
+              <p>
+                If your website suffers from poor engagement, low search engine
+                rankings or confusing brand messages - it’s hurting your
+                business. From the ground up, our custom solutions are made only
+                for you, so that you can get on with doing business, better than
+                your competitors.
+              </p>
+            </div>
+            <div id="steps" className="flex flex-row gap-6 items-start">
+              <Step
+                id={"Strategize"}
+                imageURL={"/strategize-illustration.svg"}
+                title={"Strategize"}
+                description={
+                  "We design websites that are not only visually stunning, but also convert visitors into customers."
+                }
+              />
+              <Step
+                id={"Web Design"}
+                imageURL={"/design-illustration.svg"}
+                title={"Design"}
+                description={
+                  "We design websites that are not only visually stunning, but also convert visitors into customers."
+                }
+              />
+              <Step
+                id={"Web Development"}
+                imageURL={"/develop-illustration.svg"}
+                title={"Develop"}
+                description={
+                  "We design websites that are not only visually stunning, but also convert visitors into customers."
+                }
+              />
+            </div>
+            <button className="cta-button">Upgrade Now</button>
+          </div>
+          <div>
+            <Image
+              src="/venn-diagram.svg"
+              width={800}
+              height={100}
+              alt="Web Design"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row overflow-x-scroll w-full gap-6">
+          {[1, 2, 3, 4, 5].map((num) => (
+            <ProjectTile
+              id={`Project ${num}`}
+              key={`Project ${num}`}
+              imageURL={`/Project ${num}.png`}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Step = ({ id, imageURL, title, description }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <Image src={imageURL} width={100} height={100} alt="" />
+      <h6>{title}</h6>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+const ProjectTile = ({ id, imageURL }) => {
+  return (
+    <Image key={id} src={imageURL} width={2400} height={100} alt="Web Design" />
+  );
+};
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+import { useEffect } from "react";
+
+const { default: Image } = require("next/image");
+
+export const Navbar = () => {
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const headerHeight = document.querySelector("nav").offsetHeight / 3;
+      console.log(headerHeight);
+      const nav = document.querySelector("nav");
+      const scrollY = window.scrollY;
+      console.log(scrollY > headerHeight);
+      if (scrollY > headerHeight) {
+        // nav.classList.add("scrolled");
+      } else {
+        // nav.classList.remove("scrolled");
+      }
+    });
+  });
+
+  return (
+    <nav className="w-full z-20 px-48 h-28 sticky top-0 flex flex-row justify-center items-center">
+      <ul className="w-full flex flex-row items-center justify-between rounded-full nav-inner-container">
+        {/* Logo */}
+        <div className="w-48">
+          <Image src="/logo.svg" width={32} height={32} alt="Logo" />
+        </div>
+        {/* NavLinks */}
+        <li className="flex flex-row gap-8 items-center">
+          <Link href="#works">WORK</Link>
+          <Link href="#services">SERVICES</Link>
+          <Link href="#about">ABOUT</Link>
+          <Link href="#faq">FAQ</Link>
+        </li>
+        <div className="w-48 flex flex-row justify-end">
+          <button className="cta-button-nav">CONTACT US</button>
+        </div>
+        {/* Contact Us Button */}
+      </ul>
+    </nav>
+  );
+};
