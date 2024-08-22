ec2de91c58db083db52d016d57c4cded2274beca
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-08-22 10:47:29 +0800

    change heading styles

--- src/app/components/Home/hero.jsx
+++ src/app/components/Home/hero.jsx
@@ -15,7 +15,7 @@ export const Hero = () => {
           alt="Web Design"
           className="absolute z-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
         />
-        <h1 className="hero-title text-8xl font-bold z-10 text-center">
+        <h1 className="hero-title text-7xl font-bold z-10 text-center">
           WE ARE A DESIGN FIRST COMPANY, HELPING BUSINESSES BUILD WEBSITES THAT
           CONVERT
         </h1>
