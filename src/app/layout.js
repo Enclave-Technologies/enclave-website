9770a2e603c7fb8803b1dac61208c44106e38116
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-10-18 17:43:38 +0800

    update meta tags to improve SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,16 +5,16 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title:
-    "Web & Mobile | Design & Development Studio - Hong Kong | Enclave Technologies",
-  description: "Building Modern & High Performing Software Solutions",
+  title: "Web & Mobile | Design & Development Studio in Hong Kong | Enclave",
+  description:
+    "We are a technology studio based in Hong Kong. We help businesses with web design and development, mobile app design and development and growth hacking.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ scrollBehavior: "smooth" }}>
       <Head>
-        <title>Enclave - Design & Development Studio</title>
+        <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
