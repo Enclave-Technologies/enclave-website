b07e89433d7ec9640d03f6caa3034bc2f814f42e
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-12-25 14:38:37 +0530

    add relevant meta tafs

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,10 +8,10 @@ const onest = Onest({ subsets: ["latin"] });
 export const metadata = {
   title: {
     template: "%s | Enclave",
-    default: "Enclave Studioes",
+    default: "Enclave Studios: Launch & Grow Your Digital Presence",
   },
   description:
-    "We are a technology studio based in Hong Kong. We help businesses with web design and development, mobile app design and development and growth hacking.",
+    "We are a technology studio based in Hong Kong. We help businesses launch and grow their digital presence with website development, search engine opimisation (SEO) and digital marketing services.",
 };
 
 export default function RootLayout({ children }) {
@@ -19,11 +19,51 @@ export default function RootLayout({ children }) {
     <html lang="en" style={{ scrollBehavior: "smooth" }}>
       <Head>
         <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/logo-dark.svg" />
-        <meta name="twitter:card" content="/logo-dark.svg" />
+        <meta
+          name="keywords"
+          content="Website, Design, Development, SEO, Digital, Marketing, Search, Engine, Optimisation"
+        />
+        <meta
+          property="description"
+          name="description"
+          content={metadata.description}
+        />
+        <meta property="og:type" name="og:type" content="website" />
+        <meta property="og:url" name="og:url" content="https://enclave.live" />
+        <meta property="og:title" name="og:title" content={metadata.title} />
+        <meta
+          property="og:description"
+          name="og:description"
+          content={metadata.description}
+        />
+        <meta
+          property="og:image"
+          name="og:image"
+          content="/enclave-logo-filled.png"
+        />
+        <meta
+          property="twitter:card"
+          name="twitter:card"
+          content="/enclave-logo-filled.png"
+        />
+        <meta
+          property="twitter:title"
+          name="twitter:title"
+          content="Enclave Studios: Launch & Grow Your Digital Presence"
+        />
+        <meta
+          property="twitter:description"
+          name="twitter:description"
+          content={metadata.description}
+        />
+        <meta
+          property="og:site_name"
+          name="og:site_name"
+          content="Enclave Studios"
+        />
+        <meta property="og:locale" name="og:locale" content="en_GB" />
+        <meta property="author" name="author" content="Enclave Studios" />
+        <link rel="canonical" href="www.enclave.live" />
       </Head>
       <body className={onest.className}>
         <Navbar />
