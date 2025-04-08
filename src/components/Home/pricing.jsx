18d8626f60c9f09cc7c601f748de879d42d0a8f7
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2025-04-08 23:52:53 +0800

    attempt 2 at fixing build errors

--- src/components/Home/pricing.jsx
+++ src/components/Home/pricing.jsx
@@ -2,6 +2,59 @@ import Link from "next/link";
 import { Checkmark, Cross } from "../icons";
 import { Button } from "../Button";
 
+const PricingCard = ({ pricing, highlighted }) => {
+  return (
+    <div
+      className={`w-full my-4 md:mx-4 md:my-0 flex flex-col gap-12 shadow-lg ${
+        highlighted ? "bg-[#FB4E29] -translate-y-0 text-white" : "bg-white"
+      } p-8 rounded-[24px] duration-[400ms]`}
+    >
+      <h5 className="text-4xl font-bold">{pricing.title}</h5>
+      <div className="flex flex-col gap-4">
+        {pricing.features.map((feature, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-row w-full items-center justify-between"
+            >
+              <p className="text-lg">{feature.feature}</p>
+              {feature.available ? <Checkmark /> : <Cross />}
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex flex-col gap-1">
+        <h4 className="text-5xl font-bold">{pricing.price}</h4>
+        <p className="">{pricing.additionalPrice}</p>
+      </div>
+      <Link href={"/contact"} className="" target={"_self"}>
+        <button
+          className={`primary flex flex-row items-center gap-2 min-w-fit w-full ${
+            highlighted ? "!bg-white" : ""
+          }`}
+        >
+          <span className="button-text-container min-w-fit">
+            <span
+              className={`button-text w-fit !text-lg ${
+                highlighted ? "!text-[#FB4E29]" : ""
+              }`}
+            >
+              {"Get Started"}
+            </span>
+            <span
+              className={`button-text w-fit !text-lg ${
+                highlighted ? "!text-[#FB4E29]" : ""
+              }`}
+            >
+              {"Get Started"}
+            </span>
+          </span>
+        </button>
+      </Link>
+    </div>
+  );
+};
+
 const Pricing = ({ id }) => {
   return (
     <section
@@ -82,56 +135,3 @@ const Pricing = ({ id }) => {
 };
 
 export default Pricing;
-
-const PricingCard = ({ pricing, highlighted }) => {
-  return (
-    <div
-      className={`w-full my-4 md:mx-4 md:my-0 flex flex-col gap-12 shadow-lg ${
-        highlighted ? "bg-[#FB4E29] -translate-y-0 text-white" : "bg-white"
-      } p-8 rounded-[24px] duration-[400ms]`}
-    >
-      <h5 className="text-4xl font-bold">{pricing.title}</h5>
-      <div className="flex flex-col gap-4">
-        {pricing.features.map((feature, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-row w-full items-center justify-between"
-            >
-              <p className="text-lg">{feature.feature}</p>
-              {feature.available ? <Checkmark /> : <Cross />}
-            </div>
-          );
-        })}
-      </div>
-      <div className="flex flex-col gap-1">
-        <h4 className="text-5xl font-bold">{pricing.price}</h4>
-        <p className="">{pricing.additionalPrice}</p>
-      </div>
-      <Link href={"/contact"} className="" target={"_self"}>
-        <button
-          className={`primary flex flex-row items-center gap-2 min-w-fit w-full ${
-            highlighted ? "!bg-white" : ""
-          }`}
-        >
-          <span className="button-text-container min-w-fit">
-            <span
-              className={`button-text w-fit !text-lg ${
-                highlighted ? "!text-[#FB4E29]" : ""
-              }`}
-            >
-              {"Get Started"}
-            </span>
-            <span
-              className={`button-text w-fit !text-lg ${
-                highlighted ? "!text-[#FB4E29]" : ""
-              }`}
-            >
-              {"Get Started"}
-            </span>
-          </span>
-        </button>
-      </Link>
-    </div>
-  );
-};
